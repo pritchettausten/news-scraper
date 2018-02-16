39e75f3487f3c0dc878be54e3864e1f247b76a63
Austen Pritchett <austenpritchett@austens-air.uconnect.utah.edu>
2018-02-15 18:33:20 -0700

    this

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraperdb"
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
-});
+mongoose.connect(MONGODB_URI);
 
 // mongoose.Promise = Promise;
 // mongoose.connect("mongodb://localhost/newsScraperdb", {
