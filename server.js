50cee3a483a79d785ca8b242ed051869051f380d
Austen Pritchett <austenpritchett@austens-air.uconnect.utah.edu>
2018-02-15 18:13:52 -0700

    save

--- server.js
+++ server.js
@@ -17,10 +17,21 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraperdb";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/newsScraperdb", {
-  //useMongoClient: true
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 });
+
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/newsScraperdb", {
+//   //useMongoClient: true
+// });
 
 var exphbs = require("express-handlebars");
 
